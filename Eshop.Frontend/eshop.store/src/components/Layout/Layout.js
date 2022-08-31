df701c748e93155e825ce020d30ffc0bf4046b13
andrejTavchioski <andrej.tavchioski@gmail.com>
2022-08-31 17:01:34 +0200

    add home-page

--- Eshop.Frontend/eshop.store/src/components/Layout/Layout.js
+++ Eshop.Frontend/eshop.store/src/components/Layout/Layout.js
@@ -1,22 +1,28 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { BrowserRouter,Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import "./Layout.css";
+import Footer from "../Footer/Footer";
 const Layout = () => {
   return (
     <>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {/* <Route exact path="/DNICH_TFT" element={<Home />} />
-        <Route exact path="/DNICH_TFT/champions" element={<Champions />} />
+      <BrowserRouter>
+        <Navbar />
+        <div className="main-container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* <Route exact path="/DNICH_TFT/champions" element={<Champions />} />
         <Route path="/DNICH_TFT/champions/:champ" element={<ChampionView />} />
         <Route exact path="/DNICH_TFT/items" element={<Items />} />
         <Route path="/DNICH_TFT/items/:item" element={<ItemView />} />
         <Route path="/DNICH_TFT/comps" element={<Comps />}></Route>
         <Route path="/DNICH_TFT/quiz" element={<Quiz />}></Route> */}
-      </Routes>
-    </BrowserRouter>
-  </>
+          </Routes>
+        </div>
+        <Footer></Footer>
+      </BrowserRouter>
+    </>
   );
 };
 
