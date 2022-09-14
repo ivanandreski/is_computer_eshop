906ab2a10ef284a02702703ff8a63515724a91ce
andrejTavchioski <andrej.tavchioski@gmail.com>
2022-09-14 10:49:38 +0200

    add product description design

--- Eshop.Frontend/eshop.store/src/components/Shop/ProductContainer/ProductContainer.js
+++ Eshop.Frontend/eshop.store/src/components/Shop/ProductContainer/ProductContainer.js
@@ -3,42 +3,60 @@ import tick from "../../../resources/images/green-tick.png";
 import cross from "../../../resources/images/red-cross.png";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import "./ProductContainer.css";
+import cart from "../../../resources/images/shopping-cart.png";
+import hammer from "../../../resources/images/hammer.png";
 const ProductContainer = () => {
   const [product, setProduct] = useState(null);
   const { hashId } = useParams();
 
-  const fecthProduct = async () => {
-    console.log(
-      await axios
-        .get(`https://localhost:7158/api/Product/${hashId}`)
-        .then((resp) => resp.data)
-    );
+  const fetchProduct = async () => {
+    console.log("called");
+    const product = await axios
+      .get(`https://localhost:7158/api/Product/${hashId}`)
+      .then((resp) => resp.data);
+    console.log(product);
+    setProduct(product);
   };
 
   useEffect(() => {
-    fecthProduct();
-  });
-
+    fetchProduct();
+  }, []);
+  console.log(product);
   return (
     <div className="product-container">
-      <div className="product-title">{}</div>
+      <div className="product-title">{product?.name}</div>
       <div className="product-main">
-        <div className="product-images-container"></div>
+        <div className="product-images-container">
+          <img
+            className="product-description-image"
+            src={`data:image/jpeg;base64,${product?.images[0].image}`}
+          ></img>
+        </div>
         <div className="product-main-right">
           <div className="manufacturer-stock">
-            <div>Manufacturer: {}</div>
+            <div>Manufacturer: {product?.manufacturer}</div>
             <div>
               On stock:
-              {product ? (
-                <img src={tick} alt=""></img>
+              {!product?.discontinued ? (
+                <img className="sign" src={tick} alt=""></img>
               ) : (
-                <img src={cross} alt=""></img>
+                <img className="sign" src={cross} alt=""></img>
               )}
+            </div>
+            <div className="add-to-basket">
+              Add to Basket <img src={cart}></img>
+            </div>
+            <div className="add-to-build">
+              Add to Build <img src={hammer}></img>
             </div>
           </div>
         </div>
       </div>
-      <div className="product-bottom"></div>
+      <div className="product-bottom">
+        Product Description:
+        <div id="product-description">{product?.description}</div>
+      </div>
     </div>
   );
 };
